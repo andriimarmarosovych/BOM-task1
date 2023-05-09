c178a09c42167c7b5c0fd246600122555d145668
andriimarmarosovych <andrewmartsyniuk@gmail.com>
2023-05-09 18:40:18 +0300

    first commit

---
+++ window.js
@@ -0,0 +1,17 @@
+function openWindow() {
+    
+    const newWindow = window.open("", "", "width=300, height=300");
+
+    setTimeout(function() {
+      newWindow.resizeTo(500, 500);
+    }, 2000);
+
+
+    setTimeout(function() {
+      newWindow.moveTo(200, 200);
+    }, 4000);
+
+    setTimeout(function() {
+      newWindow.close();
+    }, 6000);
+  }
